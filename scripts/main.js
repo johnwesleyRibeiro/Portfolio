174b3ff5e7ea30a3186a9fad21e5b24fb78d23fb
johnwesleyRibeiro <johnwesley12123@gmail.com>
2021-01-28 23:45:57 -0400

    atualizando

--- scripts/main.js
+++ scripts/main.js
@@ -12,24 +12,4 @@ myImage.onclick = function() {
 let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
 
-function setUserName(){
-    let myName = prompt('Por favor, insira seu nome.');
-    if (!myName) {
-        setUserName();
-    } else {
-        localStorage.setItem('name',myName);
-        myHeading.textContent = `Welcome,   ${myName}`; 
-    }
-    
-}
 
-if (!localStorage.getItem('name')) {
-    setUserName();
-} else {
-    let storedName = localStorage.getItem('name');
-    myHeading.textContent = 'Hello, ' + storedName;
-}
-
-myButton.onclick = function(){
-    setUserName();
-}
